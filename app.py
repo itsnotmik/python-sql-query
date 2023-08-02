bcd7de6e1384d590c79a05f1023115441f649c48
itsnotmik <mclement389@gmail.com>
2023-08-02 02:05:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ import pyodbc
 import os
 import pandas as pd
 
+json = []
+
 DB_SERVER = os.environ.get('DB_SERVER')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 DB_DB = os.environ.get('DB_DB')
+
+DB_SERVER = '***REMOVED***,1433'
+DB_USER = '***REMOVED***.edu'
+DB_PASS = '***REMOVED***'
+DB_DB = '***REMOVED***'
 
 db_conn = pyodbc.connect("Driver={ODBC Driver 18 for SQL Server};"
                          "Server=" + DB_SERVER +
@@ -31,7 +38,7 @@ def get_home():
 
 @app.route('/search/<string:query>', methods=['GET'])
 def get_song_names(query):
-    return get_songs(query)
+    return jsonify(get_songs(query))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
